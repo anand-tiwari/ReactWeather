23f6ad8fbf3ee4402c2f3fd28007f4c7b61cf2a9
anand <anandtiwari192@gmail.com>
2016-10-26 23:59:25 +0530

    updation of project

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,23 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About component !</h3>
+    <div>
+      <h3 className="text-center">About component !</h3>
+      <p>
+        This is weather application build using Reactjs. I have build this for learning .
+      </p>
+      <p>
+        Here are some tools that i used :
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the Javascript framework uesd.
+        </li>
+        <li>
+        <a href="https://openweathermap.org">React</a> - I used Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
+    </div>
   );
 }
 module.exports = About;
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 // var Examples = React.createClass({
 //     render: function(){
 //       return (
@@ -9,7 +9,18 @@ var React = require('react');
 // });
 var Examples = (props) => {
   return (
-    <h3>Examples component</h3>
+    <div>
+      <h3 className="text-center">Examples component</h3>
+      <p>Here are few examples to try out :</p>
+      <ol>
+        <li>
+          <Link to='/?location=Philodelphia'>Philodelphia,PA</Link>
+        </li>
+        <li>
+          <Link to='/?location=Garhwa'>Garhwa</Link>
+        </li>
+      </ol>
+    </div>
   );
 }
 module.exports = Examples;
